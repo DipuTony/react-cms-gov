1363a7fe9d024ef7fe6dfef9fa60234a2d12cf7f
Dipu Singh <dsingh197@gmail.com>
2022-07-04 23:53:21 +0530

    Single Item View

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -8,25 +8,26 @@ import AddNews from './News/AddNews';
 import ManageNews from './News/ManageNews';
 import FetchData from './News/FetchData';
 import Fetch2 from './News/Fetch2';
+import View from './News/View';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
     <React.Fragment>
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
     </React.Fragment>
   );
 }
@@ -53,25 +54,29 @@ export default function BasicTabs() {
 
   return (
     <div component={'span'} variant={'body2'}>
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Add" {...a11yProps(0)} />
-          <Tab label="Manage" {...a11yProps(1)} />
-          <Tab label="Unused" {...a11yProps(2)} />
-        </Tabs>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Add" {...a11yProps(0)} />
+            <Tab label="Manage" {...a11yProps(1)} />
+            <Tab label="Unused" {...a11yProps(2)} />
+            <Tab label="View" {...a11yProps(3)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <AddNews />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Fetch2 />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          May be used in Future..
+          <ManageNews />
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <View />
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-        <AddNews />  
-      </TabPanel>
-      <TabPanel value={value} index={1}>        
-        <Fetch2 />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        May be used in Future..
-        <ManageNews />
-      </TabPanel>
-    </Box>
     </div>
   );
 }
